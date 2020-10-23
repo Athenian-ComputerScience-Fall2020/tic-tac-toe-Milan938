da4b89a0f17760ee06e57d0888f9a79b0b196541
Milan938 <23mboga@athenian.org>
2020-10-23 17:22:17 +0000

    Not done yet

--- my_code.py
+++ my_code.py
@@ -2,15 +2,25 @@
 #  
 # A note on style: Dictionaries can be defined before or after functions.
 
+def tictactoe(board):
+    print(board['TL'] + '|' + board['TM'] + '|' + board["TR"])
+    print('--+--+--')
+    print(board['ML'] + '|' + board['MM'] + '|' + board["MR"])
+    print('--+--+--')
+    print(board['BL'] + '|' + board['BM'] + '|' + board["BR"])
+
+
 ttt = {'TL': ' ', 'TM': ' ', 'TR': ' ', 'ML': ' ', 'MM': ' ', 'MR': ' ', 'BL': ' ', 'BM': ' ', 'BR': ' '}
+
 
 print("Please select which square you'd like to place your X or O on")
 selection = input()
+plays == plays + 1
 
 if selection == 'TL':
     print("Would you like to place an X or O there?")
     choice1 = input()
-    ttt.update({'TL': choice1})
+    ttt['TL'] = choice1
 
 elif selection == 'TM':
     print("Would you like to place an X or O there?")
@@ -53,12 +63,19 @@ elif selection == 'BR':
     choice9 = input()
     ttt.update({'TM': choice9})
 
+plays = 0
+
+while plays <= 9:
+    if board['TL'] == board['TM'] == board['TR'] or board['ML'] == board['MM'] == board['MR'] or board['BL'] == board["BM"] == board['BR'] or board['TL'] == board['ML'] == board['BL'] or board['TM'] == board['MM'] == board['BM'] or board['TR'] == board['MR'] == board['BR'] or board['TL'] == board['MM'] == board['BR'] or board['TR'] == board['MM'] == board['BL']:
+        print("Congrats, you won!")
+        break
+    elif plays == 9:
+        print("No one won:(")
+        break
+
+tictactoe(ttt)
 
 
 
 
 
-
-
-
-print(ttt)
