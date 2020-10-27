1b3fb2cadcef92d321bef84361a8c4684af18954
Milan938 <23mboga@athenian.org>
2020-10-27 19:07:21 +0000

    Almost done

--- my_code.py
+++ my_code.py
@@ -12,67 +12,62 @@ def tictactoe(board):
 
 ttt = {'TL': ' ', 'TM': ' ', 'TR': ' ', 'ML': ' ', 'MM': ' ', 'MR': ' ', 'BL': ' ', 'BM': ' ', 'BR': ' '}
 
+x = 0
 
-print("Please select which square you'd like to place your X or O on")
-selection = input()
-plays == plays + 1
+while x <= 9:
+    print("Please select which square you'd like to place your X or O on")
+    selection = input()
+    if selection == 'TL':
+        print("Would you like to place an X or O there?")
+        choice1 = input()
+        ttt['TL'] = choice1
+        x = x + 1 
+    elif selection == 'TM':
+        print("Would you like to place an X or O there?")
+        choice2 = input()
+        ttt.update({'TM': choice2})
+        x = x + 1
 
-if selection == 'TL':
-    print("Would you like to place an X or O there?")
-    choice1 = input()
-    ttt['TL'] = choice1
+    elif selection == 'TR':
+        print("Would you like to place an X or O there?")
+        choice3 = input()
+        ttt.update({'TM': choice3})
+        x = x + 1
+    elif selection == 'ML':
+        print("Would you like to place an X or O there?")
+        choice4 = input()
+        ttt.update({'TM': choice4})
+        x = x + 1
+    elif selection == 'MM':
+        print("Would you like to place an X or O there?")
+        choice5 = input()
+        ttt.update({'TM': choice5})
+        x = x + 1
+    elif selection == 'MR':
+        print("Would you like to place an X or O there?")
+        choice6 = input()
+        ttt.update({'TM': choice6})
+        x = x + 1
+    elif selection == 'BL':
+        print("Would you like to place an X or O there?")
+        choice7 = input()
+        ttt.update({'TM': choice7})
+        x = x + 1
+    elif selection == 'BM':
+        print("Would you like to place an X or O there?")
+        choice8 = input()
+        ttt.update({'TM': choice8})
+        x = x + 1
+    elif selection == 'BR':
+        print("Would you like to place an X or O there?")
+        choice9 = input()
+        ttt.update({'TM': choice9})
+        x = x + 1
 
-elif selection == 'TM':
-    print("Would you like to place an X or O there?")
-    choice2 = input()
-    ttt.update({'TM': choice2})
-
-
-elif selection == 'TR':
-    print("Would you like to place an X or O there?")
-    choice3 = input()
-    ttt.update({'TM': choice3})
-
-elif selection == 'ML':
-    print("Would you like to place an X or O there?")
-    choice4 = input()
-    ttt.update({'TM': choice4})
-
-elif selection == 'MM':
-    print("Would you like to place an X or O there?")
-    choice5 = input()
-    ttt.update({'TM': choice5})
-
-elif selection == 'MR':
-    print("Would you like to place an X or O there?")
-    choice6 = input()
-    ttt.update({'TM': choice6})
-
-elif selection == 'BL':
-    print("Would you like to place an X or O there?")
-    choice7 = input()
-    ttt.update({'TM': choice7})
-
-elif selection == 'BM':
-    print("Would you like to place an X or O there?")
-    choice8 = input()
-    ttt.update({'TM': choice8})
-
-elif selection == 'BR':
-    print("Would you like to place an X or O there?")
-    choice9 = input()
-    ttt.update({'TM': choice9})
-
-plays = 0
-
-while plays <= 9:
-    if board['TL'] == board['TM'] == board['TR'] or board['ML'] == board['MM'] == board['MR'] or board['BL'] == board["BM"] == board['BR'] or board['TL'] == board['ML'] == board['BL'] or board['TM'] == board['MM'] == board['BM'] or board['TR'] == board['MR'] == board['BR'] or board['TL'] == board['MM'] == board['BR'] or board['TR'] == board['MM'] == board['BL']:
-        print("Congrats, you won!")
-        break
-    elif plays == 9:
-        print("No one won:(")
-        break
-
+if ttt['TL'] == ttt['TM'] == ttt['TR'] or ttt['ML'] == ttt['MM'] == ttt['MR'] or ttt['BL'] == ttt["BM"] == ttt['BR'] or ttt['TL'] == ttt['ML'] == ttt['BL'] or ttt['TM'] == ttt['MM'] == ttt['BM'] or ttt['TR'] == ttt['MR'] == ttt['BR'] or ttt['TL'] == ttt['MM'] == ttt['BR'] or ttt['TR'] == ttt['MM'] == ttt['BL']:
+    print("Congrats, you won!")
+elif x == 9:
+    print("No one won:( ")
 tictactoe(ttt)
 
 
