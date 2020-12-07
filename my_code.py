e2314013c775773a0a9a5dfae7bd2c88d019b506
Milan938 <23mboga@athenian.org>
2020-12-07 21:47:45 +0000

    Finished for real!

--- my_code.py
+++ my_code.py
@@ -65,63 +65,90 @@ ttt = {'TL': ' ', 'TM': ' ', 'TR': ' ', 'ML': ' ', 'MM': ' ', 'MR': ' ', 'BL': '
 
 x = 0
 
-while x <= 9:
+while x < 9:
     print("Please select which square you'd like to place your X or O on")
     selection = input()
     if selection == 'TL':
-        print("Would you like to place an X or O there?")
-        choice1 = input()
-        ttt['TL'] = choice1
-        x = x + 1 
-        winner(ttt)
+        if ttt['TL'] == ' ':
+            print("Would you like to place an X or O there?")
+            choice1 = input()
+            ttt['TL'] = choice1
+            x = x + 1 
+            winner(ttt)
+        else:
+            print("Invalid Input")
     elif selection == 'TM':
-        print("Would you like to place an X or O there?")
-        choice2 = input()
-        ttt.update({'TM': choice2})
-        x = x + 1
-        winner(ttt)
+        if ttt['TM'] == ' ':
+            print("Would you like to place an X or O there?")
+            choice2 = input()
+            ttt.update({'TM': choice2})
+            x = x + 1
+            winner(ttt)
+        else:
+            print("Invalid Input")
     elif selection == 'TR':
-        print("Would you like to place an X or O there?")
-        choice3 = input()
-        ttt.update({'TR': choice3})
-        x = x + 1
-        winner(ttt)
+        if ttt['TR'] == ' ':
+            print("Would you like to place an X or O there?")
+            choice3 = input()
+            ttt.update({'TR': choice3})
+            x = x + 1
+            winner(ttt)
+        else:
+            print("Invalid Input")
     elif selection == 'ML':
-        print("Would you like to place an X or O there?")
-        choice4 = input()
-        ttt.update({'ML': choice4})
-        x = x + 1
-        winner(ttt)
+        if ttt['ML'] == ' ':
+            print("Would you like to place an X or O there?")
+            choice4 = input()
+            ttt.update({'ML': choice4})
+            x = x + 1
+            winner(ttt)
+        else:
+            print("Invalid Input")
     elif selection == 'MM':
-        print("Would you like to place an X or O there?")
-        choice5 = input()
-        ttt.update({'MM': choice5})
-        x = x + 1
-        winner(ttt)
+        if ttt['MM'] == ' ':
+            print("Would you like to place an X or O there?")
+            choice5 = input()
+            ttt.update({'MM': choice5})
+            x = x + 1
+            winner(ttt)
+        else:
+            print("Invalid Input")
     elif selection == 'MR':
-        print("Would you like to place an X or O there?")
-        choice6 = input()
-        ttt.update({'MR': choice6})
-        x = x + 1
-        winner(ttt)
+        if ttt['MR'] == ' ':
+            print("Would you like to place an X or O there?")
+            choice6 = input()
+            ttt.update({'MR': choice6})
+            x = x + 1
+            winner(ttt)
+        else:
+            print("Invalid Input")
     elif selection == 'BL':
-        print("Would you like to place an X or O there?")
-        choice7 = input()
-        ttt.update({'BL': choice7})
-        x = x + 1
-        winner(ttt)
+        if ttt['BL'] == ' ':
+            print("Would you like to place an X or O there?")
+            choice7 = input()
+            ttt.update({'BL': choice7})
+            x = x + 1
+            winner(ttt)
+        else:
+            print("Invalid Input")
     elif selection == 'BM':
-        print("Would you like to place an X or O there?")
-        choice8 = input()
-        ttt.update({'BM': choice8})
-        x = x + 1
-        winner(ttt)
+        if ttt['BM'] == ' ':
+            print("Would you like to place an X or O there?")
+            choice8 = input()
+            ttt.update({'BM': choice8})
+            x = x + 1
+            winner(ttt)
+        else:
+            print("Invalid Input")
     elif selection == 'BR':
-        print("Would you like to place an X or O there?")
-        choice9 = input()
-        ttt.update({'BR': choice9})
-        x = x + 1
-        winner(ttt)
+        if ttt['BR'] == ' ':
+            print("Would you like to place an X or O there?")
+            choice9 = input()
+            ttt.update({'BR': choice9})
+            x = x + 1
+            winner(ttt)
+        else:
+            print("Invalid Input")
 
 if x == 9:
     print("No one won:( ")
